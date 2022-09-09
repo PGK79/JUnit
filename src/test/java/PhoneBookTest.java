8e0ba26321e244a5282c570cd7e6369ab808147c
PGK79 <pgk_nev@mail.ru>
2022-09-09 11:10:14 +0300

    Добавлен файл PhoneBookTest.java

---
+++ src/test/java/PhoneBookTest.java
@@ -0,0 +1,92 @@
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.*;
+
+public class PhoneBookTest {
+
+    static PhoneBook sut;
+
+    @BeforeAll
+    public static void startedAll() {
+        System.out.println("Начало тестов");
+        sut = new PhoneBook();
+    }
+
+    @BeforeEach
+    public void InitAndStart() {
+        System.out.println("Старт теста");
+        sut = new PhoneBook();
+    }
+
+    @AfterAll
+    public static void finishAll() {
+        System.out.println("Все тесты завершены");
+    }
+
+    @AfterEach
+    public void finished() {
+        System.out.println("Тест завершен");
+    }
+
+    @Test
+    //Тест академический. С практической точки бесмыссленный, т.к мапа не будет пустой
+    // если список не пустой.
+    public void testContactPut_successfulRecordingList() {
+        // given:
+        final String argName = "Константин";
+        final String argTel = "12345";
+        final List<Contact> actual = new ArrayList<>();
+
+        // when:
+        sut.contactPut(argName, argTel);
+
+        // then:
+        Assertions.assertNotNull(actual);
+    }
+
+    @Test
+    public void testContactPut_successfulRecordingMap() {
+        // given:
+        final String argName = "Константин";
+        final String argTel = "12345";
+
+        // when:
+        sut.contactPut(argName, argTel);
+
+        // then:
+        Assertions.assertNotNull(sut);
+    }
+
+    @Test
+    public void testAddGroup_successfulRecordingMap() {
+        // given:
+        final String group = "Друзья";
+
+        // when:
+        sut.addGroup(group); // вызов сопровождается выводом строки в консоль
+                             // пока не разобрался как запретить вывод в тесте,
+                            //  сохранив вывод в основном классе
+        // then:
+        Assertions.assertNotNull(sut);
+    }
+
+    @Test
+    public void TestGetContacts_arrayReturnCheck() {
+        // given:
+        Contact contact = new Contact("Костя", "111");
+        sut.contactPut("Костя", "111");
+        sut.addGroup("Семья");
+        final String[] groups = {"Семья"};
+        sut.addContact(contact, groups);
+        final String argument = "Семья";
+        final Contact[] expected = {contact};
+
+        // when:
+        Contact[] actual = sut.getСontacts(argument);
+
+        // then:
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
